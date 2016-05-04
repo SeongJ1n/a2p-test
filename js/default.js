d5bf97770a8d4848d508f80ef82b960af50677aa
SeongJ1n <happy8721@naver.com>
2016-05-04 16:09:37 +0900

    맵 마커 테스트 / for문 나중에 수정필요

--- js/default.js
+++ js/default.js
@@ -1,29 +1,40 @@
-/* Daum map */
 
+var univ = '007030';
 
+var latitude = coord_data[univ][0].latitude;
+var longitude = coord_data[univ][0].longitude;
+
+/* Daum map 생성 */
 var container = document.getElementById('map');
 var options = {
-  center: new daum.maps.LatLng(37.498, 127.0276),
+  center: new daum.maps.LatLng(latitude, longitude),
   level: 3
 };
 
 var map = new daum.maps.Map(container, options);
 
-var content = '<div class="arrow-box">' +
-              '<h5 class="arrow-box-placename">문과대도서관</h5>' +
-              '<p class="arrow-box-pcnumber">PC 4대</p>'
-              '</div>';
 
+/* 마커 생성 */
 
+for(a=0; a<=7; a++){
+  var name = coord_data[univ][a].name;
+  var count = coord_data[univ][a].count;
+  var latitude = coord_data[univ][a].latitude;
+  var longitude = coord_data[univ][a].longitude;
 
+  var content = '<div class="arrow-box">'+
+                '<h5 class="arrow-box-placename">'+name+'</h5>' +
+                '<p class="arrow-box-pcnumber">PC '+count+'대</p>'
+                '</div>';
 
-var position = new daum.maps.LatLng(37.498, 127.0276);
+  var position = new daum.maps.LatLng(latitude, longitude);
 
-var customOverlay = new daum.maps.CustomOverlay({
-    position: position,
-    content: content,
-    xAnchor: 0.3,
-    yAnchor: 0.91
-});
+  var customOverlay = new daum.maps.CustomOverlay({
+      position: position,
+      content: content,
+      xAnchor: 0,
+      yAnchor: 0
+  });
 
-customOverlay.setMap(map);
+  customOverlay.setMap(map);
+}
